0f6d80e3751a4a5dd7108fb602ed37dbbdb76ce2
ManoharK <hellomanoharkr@gmail.com>
2024-05-07 11:21:37 +0530

    Object detection in video file

---
+++ CMakeFiles/Video_Obj_Detection.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hstpl_lap_131/this/openCv/Yolov5_Video_Object_Detection/Video_Obj_Detection.cpp" "CMakeFiles/Video_Obj_Detection.dir/Video_Obj_Detection.cpp.o" "gcc" "CMakeFiles/Video_Obj_Detection.dir/Video_Obj_Detection.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Video_Obj_Detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Video_Obj_Detection.dir/Video_Obj_Detection.cpp.o"
+  "CMakeFiles/Video_Obj_Detection.dir/Video_Obj_Detection.cpp.o.d"
+  "Video_Obj_Detection"
+  "Video_Obj_Detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Video_Obj_Detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
